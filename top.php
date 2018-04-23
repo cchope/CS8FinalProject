d99502fb638938b712ea84fdfe3530d723c66b67
cchope <36555395+cchope@users.noreply.github.com>
2018-04-23 17:34:00 -0400

    Update and rename top to top.php

--- top.php
+++ top.php
@@ -1,10 +1,5 @@
 <?php
-// parse the url into htmlentities to remove any suspicious vales that someone
-// may try to pass in. htmlentities helps avoid security issues.
-
 $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
-
-// break the url up into an array, then pull out just the filename
 $path_parts = pathinfo($phpSelf);
 
 
@@ -12,17 +7,16 @@ $path_parts = pathinfo($phpSelf);
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- you can add php code here (similar to nav.php) to print a different title on each page -->
-        <title>Your relationship with Climate Change </title>
+        <title> On Campus Delivery </title>
 
         <meta charset="utf-8">
         <meta name="author" content="Stanhope N., Clasby C., Eddie T.">
-        <meta name="description" content=" ">
+        <meta name="description" content="On Campus Delivery">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel=stylesheet
               type="text/css" 
-              href="../css/custom.css?<?php print time(); ?>"  
+              href="style.css"  
               media="screen"
               >
               <?php
@@ -60,12 +54,12 @@ $path_parts = pathinfo($phpSelf);
               ?>
 
     </head>
-
+    <header>
+    <h1> On Campus Delivery
+    </header>
     <?php
-// giving each body tag an id really helps with css later on
     print '<body id="' . $path_parts['filename'] . '">';
-
-    include ('header.php');
+    
     include ('nav.php');
 
     if ($debug) {
