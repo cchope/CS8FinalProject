dbcad190b16b44576ed138bfc8319fa420378375
cchope <36555395+cchope@users.noreply.github.com>
2018-04-23 17:27:22 -0400

    Update and rename Intro-Page to index.php

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 <html lang="en">
 
 <? 
-include("top.php");
-include("head.php");
-include("nav.php");
->
-
+include('top.php');
+?>
     <head>
         <title> Delivery On Campus </title>
         <meta charset="utf-8">
@@ -16,23 +13,26 @@ include("nav.php");
     
     
     <body>
-    <ul>
-            <li><a href="developers.php"> The developers. </a></li>
-    </ul>
+    <h1> Delivery On Campus </h1>
+    <p id="introduction">
+     This paragraph should introduce the concept of this page
+    </p>
     
+
     <h1> Clubs/Organizations </h1>
-      
+    <p> Below are a list of clubs and organizations that are currently served by Delivery On Campus </p>
+    
         <ol>
           <li><a href="feel-good.php"> FeelGood </a></li>
           <li><a href="intervarsity.php"> InterVarsity </a></li>
-          <li><a href=" .php"> Club 3 </a></li>
+          <li><a href="amnesty.php"> Amnesty International </a></li>
+           <li><a href="outinglclub.php"> The Outing Club </a></li>
         </ol>
-    </body>
-</html> 
-><
-
-
+ 
 <? 
 include("footer.php");
 >
+    </body>
+</html> 
+
 
