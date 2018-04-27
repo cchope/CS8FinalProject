a80141ad1d2bf32f124574f43dc59fec2c7981d3
bobsclass <38634952+bobsclass@users.noreply.github.com>
2018-04-27 13:23:42 -0400

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,26 @@
+<?php
+include 'top.php';
+?>
+<img src="images/alana gear.png" alt="Alana Gear retreat" class='pic'>
+<h1>Welcome!</h1>
+<p>Welcome to UVMeats!  This page brings together all clubs on campus who are trying to bring food to hungry UVM students.
+    Above you will find the page navigation which will lead you food options from different clubs.  Options range from 
+    cookies to donuts to nachos to even grilled cheese!  Explore the page and place your order today!</p>
+
+<img src="images/outing club.png" alt="The outing club" class='pic2'>
+<h2>Why are we doing this?</h2>
+<p>So I know you've walked in your building and have seen fliers about food on campus.  And although it works well its not efficient.  Fliers 
+fall off or people can rip them off the wall.  Also what if you saw a flier and forgot to save the number now you have to leave your room 
+head to the floor where you saw it and text them.  Thats too much work, we're in college we are lazy.  With UVMeats you have food literally at 
+your finger tips and orders arrive within 20 minutes. </p>
+
+
+<h2> Clubs involved </h2> 
+<p>We have collaborated with four clubs Intervarsity, Feel Good, The Outing Club, and Alana Gear.  These clubs are all selling different
+   foods for your consumption.   </p>
+
+<img src="images/intervarsity.jpeg" alt="Intervarsity Group photo">
+
+<?php 
+include'footer.php';
+?>
