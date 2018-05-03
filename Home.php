9b1567432cd699a93e8d8fdbceb410f7cb52d9a9
yugiohxl <38139912+yugiohxl@users.noreply.github.com>
2018-05-03 14:01:01 -0400

    Update Home.php

--- Home.php
+++ Home.php
@@ -19,14 +19,27 @@ your finger tips and orders arrive within 20 minutes. </p>
 <p>We have collaborated with four clubs already to help spread the love and great taste across the campus. Some of these clubs and organziations include Intervarsity, Feel Good, The Outing Club, and Alana Gear. 
    They each have made food, swwets and some delectable treats for any campus and UVM student that they can help. 
     
-    For example, Intervarsity is out and about, baking sheets of cookies, chcolatee chip to snickerdoodles.
-    Our own on-campus Christain misnrty group bakes and they can bake well! 
+    For example, Intervarsity is out and about, baking sheets of cookies, chcolatee chip to snickerdoodles. Our own on-campus Christain misnrty group bakes and they can bake well! 
     They usually get overcrowed in orders for their cookies, some of which they also give out for some that could not afford even a half dozen. 
-    Truly a group with big hearts and even sweeter cookies! They typically have their late night cookie orders up for take starting from 9pm all the way to midnight on the dot. The next up and coming days for deliveries are set for the 2nd and 4th Fridays and Saturdays of September and November and the 3rd weekend of October. Remember when your feeling the past 8pm cookie hunger.
+    Truly a group with big hearts and even sweeter cookies! They typically have their late night cookie orders up for take starting from 9pm all the way to midnight on the dot.
+    The next up and coming days for deliveries are set for the 2nd and 4th Fridays and Saturdays of September and November and the 3rd weekend of October. Remember when your feeling the past 8pm cookie hunger.
+    And for only $3 for half a dozen and a full dozen for only $5, let the Cookie Monsters eat!
 
     
+    When people think about cheese, their minds are off to grilled cheese sandwiches. And when you think about grilled cheeses, none other than Feel Good comes to the minds of every UVM campus student. 
+    They make their sandwiches with love, flavor and all the best things a sandwich can need. Nothing brings a lunch/snack hungry college student the joy their grilled cheeses bring. 
+    With a Plane jane for simple bite or a nice Maple Staple for a treat-yo-self day, Feel Good got what you need. And what's more, it's all donation based, with the recommendation of only $4-5 per sandwich. 
+    At theses prices, why wouldn't you grab one while they last. 
     
-    Another favorite for feeding the campus is Alana Gear. Not only do they know how to hike, climb and bblaze them forest trails, but they also know how 
+    
+    Another favorite for hungry college kids with a good sweet tooth, Alana Gear. They love their hikes, climbs and trail blazing and love glazing sme donuts too. 
+    They have a variety of tasty donuts that you can imagine. Mainly of the krispy kremes to Dunkin Ds, let's not forget the nice and local Koffee Kup Bakery. 
+    Running over the treacherous campus, hindered by neither rain, nor shine, slush or mud, they’ve set to reach for your donut needs. 
+    With a half dozen for $5 and and whole baked dozen for $8, these are deals that cannot be overlooked.
+    
+    
+    
+
     
     
     But please do not think that we will only keep just to these few groups. 
